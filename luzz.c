38ef4f943714ee24e4b4e7e2938ac2a1a2c66c86
Bart Van Der Meerssche <bart.vandermeerssche@flukso.net>
2014-05-28 10:42:24 +0200

    [mosq] include hostname in client id

--- luzz.c
+++ luzz.c
@@ -30,6 +30,7 @@
 #include <linux/spi/spidev.h>
 #include <sched.h>
 #include <sys/mman.h>
+#include <sys/utsname.h>
 #include <mosquitto.h>
 
 #include "luzz.h"
@@ -240,7 +241,13 @@ main(int argc, char *argv[])
 
 	mosquitto_lib_init();
 
-	if ((rc = asprintf(&mqtt.id, LUZZ_ID_TPL, LUZZ_VERSION, ctx.panel, getpid())) < 0) {
+	if (uname(&ctx.uts) < 0) {
+		perror("uname");
+		goto finish;
+	}
+
+	if ((rc = asprintf(&mqtt.id, LUZZ_ID_TPL,
+	        LUZZ_VERSION, ctx.panel, ctx.uts.nodename, getpid())) < 0) {
 		goto oom;
 	}
 	mosq = mosquitto_new(mqtt.id, mqtt.clean_session, &ctx);
